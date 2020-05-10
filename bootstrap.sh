25b57e8aad5fd3f16555699fe3a0b2693de5891a
Paolo Sacconier <paolo@sacconier.net>
2020-05-11 00:46:18 +0200

    expose the local docker socket

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ docker build . -t jenkins-as-code && \
 docker run -d -p 49001:8080 \
     --name jenkins-as-code_1 \
     --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v $PWD/jenkins:/var/jenkins_home:z \
     -v $PWD:/application/jenkins-as-code \
     -t \
